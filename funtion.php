49b9cf88ffd69019dfb59d398e2e9b4a5c076b55
Dew <158989279+JakrayuSaithong@users.noreply.github.com>
2026-03-10 10:09:41 +0700

    Update funtion.php

--- funtion.php
+++ funtion.php
@@ -1,8 +1,1 @@
-<?php
-    function decryptIt($q)
-    {
-        $cryptKey  = 'Iloveyouallpann';
-        $qDecoded = rtrim(openssl_decrypt(base64_decode($q), "AES-256-CBC", md5($cryptKey), 0, substr(md5(md5($cryptKey)), 0, 16)), "\0");
-        //write_file("temp.txt","=".date("H:i:s")."=qDecoded===$qDecoded=\r\n","a");
-        return ($qDecoded);
-    }
+
